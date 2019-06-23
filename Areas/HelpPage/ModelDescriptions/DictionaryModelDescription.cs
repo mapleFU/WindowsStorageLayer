c77f0e153351860689b3dfe08292040815c8c011
mwish <1506118561@qq.com>
2019-06-23 20:12:56 +0800

    [Init] Init project with vs ASP.NET framework web api

---
+++ Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WindowsStorageLayer.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WindowsStorageLayer.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
